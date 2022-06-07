6863532ef116bf8aa81d729ce4032de196a49ff2
isamo <isamo@softserveinc.com>
2022-06-07 18:47:25 +0300

    Add one-to-many association between users & articles from back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article]
     @article = Article.new(article_params)
+    @article.user = User.first
     # render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article with id=#{@article.id} was created successfully!"
