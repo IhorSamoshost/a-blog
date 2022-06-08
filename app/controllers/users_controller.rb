3fa69bd2965b99d9c83412895fbbcd0c8a0dd872
isamo <isamo@softserveinc.com>
2022-06-08 19:59:31 +0300

    Add authentication helper methods, modified navbar for login-logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alf Blog, #{@user.username}! You have successfully signed up!"
       redirect_to articles_path
     else
