c0ac240398dd5229639277e3daafbdd58c4b253a
isamo <isamo@softserveinc.com>
2022-06-09 18:26:14 +0300

    Add create action in categories_controller & its functional test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,26 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category #{ @category.name } was successfully created!"
+      redirect_to @category
+    else
+      flash[:alert] = "Category is not created - something went wrong, try again!"
+      render "new"
+    end
+  end
+
+  private
+
+  def set_category
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
